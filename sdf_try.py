1f8eae170fa8142ba7edacf53da13f2d14f6b132
Asar5 <asara@oregonstate.edu>
2020-05-14 19:22:19 -0700

    First try of signed distance function using trimesh

---
+++ sdf_try.py
@@ -0,0 +1,29 @@
+"""
+sdf_try.py
+----------------
+
+Given a mesh and a point, find the  signed distance function.
+OUTSIDE --> NEGETIVE
+INSIDE + WITHIN TOLERANCE --> POSITIVE
+"""
+
+import trimesh
+
+if __name__ == '__main__':
+
+    mesh = trimesh.load(trimesh.primitives.Sphere())#../models/Alan_4.stl')#trimesh.primitives.Sphere()
+
+    # arbitrary indices to test with
+    start = [[0,0,0]]#, [0,1,1], [1,1,1]]
+
+    distance = trimesh.proximity.signed_distance(mesh, start)
+    print (distance)
+
+    # VISUALIZE RESULT
+    # make the sphere transparent-ish
+    mesh.visual.face_colors = [100, 100, 100, 100]
+    # visualizable two points
+    points_visual = trimesh.points.PointCloud(start)
+    # create a scene with the mesh, path, and points
+    scene = trimesh.Scene([points_visual, mesh])
+    (scene).show(smooth=False)
